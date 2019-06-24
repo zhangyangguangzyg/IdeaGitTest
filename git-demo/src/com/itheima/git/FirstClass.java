cda3dd48258410d15a25ce4cd2b5f50d56cd33a5
zhangsan <123@qq.com>
2019-06-24 16:20:03 +0800

    clone then change first

--- git-demo/src/com/itheima/git/FirstClass.java
+++ git-demo/src/com/itheima/git/FirstClass.java
@@ -10,9 +10,11 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+        //hello world
     }
     public void test(){
         System.out.println("000");
         System.out.println("002");
+        System.out.println("003");
     }
 }
